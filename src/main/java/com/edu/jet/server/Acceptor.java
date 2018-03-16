6acbaef63a0c7f5853bd43cccdb215eae75e0cfc
MrB52 <bondar2504@mail.ru>
2018-03-16 18:25:55 +0300

    final-final2

--- src/main/java/com/edu/jet/server/Acceptor.java
+++ src/main/java/com/edu/jet/server/Acceptor.java
@@ -15,7 +15,7 @@ import java.util.concurrent.Executors;
 
 public class Acceptor implements Runnable {
     public static HashSet<ClientSession> aliveSocketList = new HashSet<>();
-    private final int MAX_ONLINE = 10;
+    private final int MAX_ONLINE = 1000;
     public static Saver saver = new MemorySaver();
 
     @Override
